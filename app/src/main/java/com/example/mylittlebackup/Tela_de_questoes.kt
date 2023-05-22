8a0cf53b87535d13cbcf5e92721141e790eed715
RafaelSS96 <107450960+RafaelSS96@users.noreply.github.com>
2023-05-22 18:56:33 -0300

    0.2.0

--- app/src/main/java/com/example/mylittlebackup/Tela_de_questoes.kt
+++ app/src/main/java/com/example/mylittlebackup/Tela_de_questoes.kt
@@ -1,11 +1,86 @@
 package com.example.mylittlebackup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class Tela_de_questoes : AppCompatActivity() {
+    private lateinit var textopergunta: TextView
+    private lateinit var b1: Button
+    private lateinit var b2: Button
+    private lateinit var b3: Button
+    private lateinit var b4: Button
+
+    private val perguntas_respostas = mapOf(
+        "Qual forma geometrica possui 6 lados?" to listOf(
+            Pair("hexagono", 10),
+            Pair("pentagono", 0),
+            Pair("Hectagono", 0),
+            Pair("Sixagono", 0)
+        )
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_de_questoes)
+
+        textopergunta = findViewById(R.id.questao)
+        b1 = findViewById(R.id.resposta1)
+        b2 = findViewById(R.id.resposta2)
+        b3 = findViewById(R.id.resposta3)
+        b4 = findViewById(R.id.resposta4)
+
+
+        var perguntaAleatoria = perguntas_respostas.keys.random()
+
+        textopergunta.text = perguntaAleatoria
+
+        val respostas = perguntas_respostas[perguntaAleatoria]
+
+        val respostasEmbaralhadas = respostas?.shuffled()
+
+        b1.text = respostasEmbaralhadas?.get(0)?.component1()
+        b2.text = respostasEmbaralhadas?.get(1)?.component1()
+        b3.text = respostasEmbaralhadas?.get(2)?.component1()
+        b4.text = respostasEmbaralhadas?.get(3)?.component1()
+
+        b1.setOnClickListener {
+
+            val respostaSelecionada = respostasEmbaralhadas?.get(0)?.component2()
+
+            respostacorreta(respostaSelecionada!!.toInt())
+        }
+
+        b2.setOnClickListener {
+            val respostaSelecionada = respostasEmbaralhadas?.get(1)?.component2()
+
+            respostacorreta(respostaSelecionada!!.toInt())
+        }
+        b3.setOnClickListener {
+            val respostaSelecionada = respostasEmbaralhadas?.get(2)?.component2()
+
+            respostacorreta(respostaSelecionada!!.toInt())
+        }
+        b4.setOnClickListener {
+            val respostaSelecionada = respostasEmbaralhadas?.get(3)?.component2()
+
+            respostacorreta(respostaSelecionada!!.toInt())
+        }
+
+
     }
+
+    fun respostacorreta(valor: Int) {
+        if (valor == 10) {
+            return Toast.makeText(this, "Resposta correta!", Toast.LENGTH_SHORT).show()
+
+        } else {
+            return Toast.makeText(this, "resposta errada :/", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/mylittlebackup/Tela_principal.kt
+++ app/src/main/java/com/example/mylittlebackup/Tela_principal.kt
@@ -1,11 +1,46 @@
 package com.example.mylittlebackup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
 
 class Tela_principal : AppCompatActivity() {
+
+    private lateinit var novaatividade: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_principal)
+
+        novaatividade = findViewById(R.id.novaatividade)
+
+        novaatividade.setOnClickListener {
+            val intent = Intent(this,Tela_de_questoes::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_pagina_principal, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.configuracao -> {
+                return true
+            }
+
+            R.id.deslogar -> {
+                return true
+            }
+        }
+        return true
     }
 }
